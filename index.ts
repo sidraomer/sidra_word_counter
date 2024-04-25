56a5736c52e3d3945af277b553b0e3ee2efb8d65
sidraomer <162823813+sidraomer@users.noreply.github.com>
2024-04-26 00:32:47 +0500

    upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+// Word Counter CLI Program
+
+import inquirer from "inquirer";
+
+let data = await inquirer.prompt(
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence / paragraph: "
+    }
+)
+
+let words = data.sentence.split(" ")
+
+    console.log("Total Words With a Space:", words.length);
+
+let characters = words.join("")
+
+    console.log("Total Characters Without Space:" , characters.length)
